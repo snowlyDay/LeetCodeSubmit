a50afb7f86befff67994c2bb1490023cda5a14e2
yinbao <snowlydaymail@163.com>
2022-11-20 23:28:57 +0800

    say num

--- LeetCode.cs
+++ LeetCode.cs
@@ -1,49 +1,20 @@
-private void OnPostprocessModel(GameObject g)
-{
-    var expectedMaterial = AssetDatabase.LoadAssetAtPath<Material>("Assets/custom_m.mat");
-    using (var so = new SerializedObject(assetImporter))
-    {
-        var materials = so.FindProperty("m_Materials");
-        var externalObjects = so.FindProperty("m_ExternalObjects");
-
-        for (int materialIndex = 0; materialIndex < materials.arraySize; materialIndex ++)
-        {
-            var id = materials.GetArrayElementAtIndex(materialIndex);
-            var name = id.FindPropertyRelative("name").stringValue;
-            var type = id.FindPropertyRelative("type").stringValue;
-            var assembly = id.FindPropertyRelative("assembly").stringValue;
-
-            SerializedProperty materialProperty = null;
-
-            for (int externalObjectIndex = 0; externalObjectIndex < externalObjects.arraySize; externalObjectIndex ++)
-            {
-                var currentSerializedProperty = externalObjects.GetArrayElementAtIndex(externalObjectIndex);
-                var externalName = currentSerializedProperty.FindPropertyRelative("first.name").stringValue;
-                var externalType = currentSerializedProperty.FindPropertyRelative("first.type").stringValue;
-
-                if (externalType == type && externalName == name)
-                {
-                    materialProperty = currentSerializedProperty.FindPropertyRelative("second");
-                    break;
-                }
-            }
-
-            if (materialProperty == null)
-            {
-                var lastIndex = externalObjects.arraySize++;
-                var currentSerializedProperty = externalObjects.GetArrayElementAtIndex(lastIndex);
-                currentSerializedProperty.FindPropertyRelative("first.name").stringValue = name;
-                currentSerializedProperty.FindPropertyRelative("first.type").stringValue = type;
-                currentSerializedProperty.FindPropertyRelative("first.assembly").stringValue = assembly;
-                currentSerializedProperty.FindPropertyRelative("second").objectReferenceValue = expectedMaterial;
-            }
-            else
-            {
-                materialProperty.objectReferenceValue = expectedMaterial;
+public class Solution {
+    public string CountAndSay(int n) {
+        if (n == 1) return "1";
+        var prev = CountAndSay(n - 1);
+        var sb = new StringBuilder();
+        var count = 1;
+        for (var i = 1; i < prev.Length; i++) {
+            if (prev[i] == prev[i - 1]) {
+                count++;
+            } else {
+                sb.Append(count);
+                sb.Append(prev[i - 1]);
+                count = 1;
             }
         }
-
-        so.ApplyModifiedPropertiesWithoutUndo();
+        sb.Append(count);
+        sb.Append(prev[prev.Length - 1]);
+        return sb.ToString();
     }
-    assetImporter.SaveAndReimport();
 }
